df085a234cab58f5aa953dbfc49e19212ab22610
Nan Rosenbloom <nanr@ucar.edu>
2020-07-24 12:41:16 -0600

    fixing setup to 5 mbrs

--- scripts/setup_wedge_pacemaker_cylc.sh
+++ scripts/setup_wedge_pacemaker_cylc.sh
@@ -14,10 +14,10 @@ INITNAME="b.e21.BHISTcmip6.f09_g17.LE2-"
 SOURCEMODSDIR="/glade/work/nanr/cesm_tags/CASE_tools/cesm2-pacemaker/"
 
 STARTMEMBER=1
-ENDMEMBER=2
+ENDMEMBER=5
 
 CASEROOTBASE="/glade/work/nanr/CESM2-pacemaker/cases/" # !!change this 
-BASENAME=b.e21.$COMPSET'.'$GRID'.pacemaker_pacific_TESTAGAIN1'
+BASENAME=b.e21.$COMPSET'.'$GRID'.pacemaker_pacific_'
 SCRATCHBASE="/glade/scratch/nanr/" # !!!change this
 
 cd ~nanr/CESM-WF/
