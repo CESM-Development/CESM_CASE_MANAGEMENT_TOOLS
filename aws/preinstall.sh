08507b8e711a885b3179790f7d59e3ec5fa5d7d4
briandobbins <bdobbins@ucar.edu>
2021-08-02 18:32:11 +0000

    Added RAW github links for testing scripts

--- aws/preinstall.sh
+++ aws/preinstall.sh
@@ -4,11 +4,11 @@
 
 case "${cfn_node_type}" in
     MasterServer)
-	curl -o master_install.sh <remote URL>
+	curl -o master_install.sh https://raw.githubusercontent.com/CESM-Development/CESM_CASE_MANAGEMENT_TOOLS/cesm2-waccm/aws/master_install.sh
 	sh master_install.sh > /tmp/master_install.log
     ;;
     ComputeFleet)
-	curl -o compute_install.sh 
+	curl -o compute_install.sh https://raw.githubusercontent.com/CESM-Development/CESM_CASE_MANAGEMENT_TOOLS/cesm2-waccm/aws/compute_install.sh
 	sh compute_install.sh > /tmp/compute_install.log
     ;;
     *)
