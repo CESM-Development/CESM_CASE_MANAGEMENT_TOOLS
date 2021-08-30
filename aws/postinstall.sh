1b15f27e52cc387e060f13a778406a57f87d269a
Brian Dobbins <bdobbins@ucar.edu>
2021-08-30 14:57:11 +0000

    More changes to get around lack of routing on compute nodes

--- aws/postinstall.sh
+++ aws/postinstall.sh
@@ -8,8 +8,22 @@ case "${cfn_node_type}" in
 	sh master_install.sh > /tmp/master_install.log
     ;;
     ComputeFleet)
-	curl -o compute_install.sh https://raw.githubusercontent.com/CESM-Development/CESM_CASE_MANAGEMENT_TOOLS/cesm2-waccm/aws/compute_install.sh
-	sh compute_install.sh > /tmp/compute_install.log
+    # Add the 'cesm' group
+    groupadd cesm
+
+    # add the 'geostrat' user (later, this functionality will be handled by the API)
+    # We'll check if the /home/geostrat directory exists and call with -M if it does:
+    if [ -d /home/geostrat ]; then
+      adduser -c "NCAR GeoStrat" -d /home/geostrat -g cesm -M -s /bin/bash geostrat
+    fi
+
+    ln -s /usr/bin/python3 /usr/bin/python
+
+    # Set up our search path
+    echo '/opt/ncar/software/lib' > /etc/ld.so.conf.d/ncar.conf
+
+    # Also add the compilers to the /etc/profile.d/oneapi.sh
+    echo 'source /opt/intel/oneapi/setvars.sh > /dev/null' > /etc/profile.d/oneapi.sh
     ;;
     *)
     ;;
