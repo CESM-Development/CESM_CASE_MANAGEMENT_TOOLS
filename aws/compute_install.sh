bf34123869bf4a173443fee5569822edffc296f5
Brian Dobbins <bdobbins@ucar.edu>
2021-08-30 14:54:24 +0000

    Argh - AWS doesn't allow compute nodes traffic without using the NAT Gateway ($), so disabling remote calls on compute node installs

--- aws/compute_install.sh
+++ aws/compute_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install -y lapack blas
+#yum install -y lapack blas
 
 # Set up the 'python' alias to point to Python3 -- this is going away for newer CESM releases, I think, but may
 # be needed for this 2.1.4-rcX version
@@ -12,6 +12,6 @@ echo '/opt/ncar/software/lib' > /etc/ld.so.conf.d/ncar.conf
 # Also add the compilers to the /etc/profile.d/oneapi.sh
 echo 'source /opt/intel/oneapi/setvars.sh > /dev/null' > /etc/profile.d/oneapi.sh
 
-cd /tmp
-wget -q https://raw.githubusercontent.com/CESM-Development/CESM_CASE_MANAGEMENT_TOOLS/cesm2-waccm/aws/add_geostrat.sh
-sh add_geostrat.sh
+#cd /tmp
+#wget -q https://raw.githubusercontent.com/CESM-Development/CESM_CASE_MANAGEMENT_TOOLS/cesm2-waccm/aws/add_geostrat.sh
+#sh add_geostrat.sh
