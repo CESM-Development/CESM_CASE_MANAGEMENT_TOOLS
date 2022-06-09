7e5f69d0b54ca24834481ccc43fda32b94f94156
Nan Rosenbloom <nanr@ucar.edu>
2022-06-08 19:42:38 -0700

    pre-EAM fixes

--- scripts/SSP/v2.LR.LE_extra.sh
+++ scripts/SSP/v2.LR.LE_extra.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #echo ========================================================
-#n="0231"
+#n="0241"
 #echo Attempting to restart ${n}
 #date
-#cd /global/cscratch1/sd/golaz/E3SMv2/v2.LR.historical_${n}/case_scripts
+#cd /global/cscratch1/sd/nanr/E3SMv2/v2.LR.SSP370_${n}/case_scripts
 #./xmlchange run_exe="--kill-on-bad-exit=1 --job-name=${n} \${EXEROOT}/e3sm.exe "
-#./xmlchange STOP_N="6"
+#./xmlchange STOP_N="5"
 #./case.submit --no-batch 2>&1 > ../log.o${SLURM_JOB_ID} &
 #PID=$!
 #echo $PID
