ab734d190011fa0cd57285cebcc4776113c8d195
Nan Rosenbloom <nanr@ucar.edu>
2026-07-06 17:40:00 -0600

    update repo

--- scripts/setup_wedge_pacemaker_SSP370.derecho.sh
+++ scripts/setup_wedge_pacemaker_SSP370.derecho.sh
@@ -6,22 +6,20 @@
 
 COMPSET=BSSP370cmip6
 GRID=f09_g17
-CESMROOT="/glade/work/islas/cesm2.1.3/"
+CESMROOT="/glade/work/nanr/cesm_tags/cesm2.1.5/"
 PROJECT=P93300313
-INITDIR="/glade/scratch/asphilli/archive/" # directory containing restart files from first 10 macro members of CESM2-LE
-INITNAME="b.e21.BHISTcmip6.f09_g17.pacemaker_pacific"
+INITDIR="/glade/derecho/scratch/nanr/archive/" # directory containing restart files from first 10 macro members of CESM2-LE
+INITNAME="b.e21.BSSP370cmip6.f09_g17.pacemaker_pacific"
 #SOURCEMODSDIR="/glade/u/home/islas/CVCWG/CESM2_PACEMAKER/CESM2_SourceMods/"
 SOURCEMODSDIR="/glade/work/nanr/cesm_tags/CASE_tools/cesm2-pacemaker/"
 
-STARTMEMBER=6
+STARTMEMBER=3
 ENDMEMBER=10
 
 CASEROOTBASE="/glade/work/nanr/CESM2-pacemaker/cases/" # !!change this 
-BASENAME=b.e21.$COMPSET'.'$GRID'.pacemaker_pacific'
-SCRATCHBASE="/glade/scratch/nanr/" # !!!change this
+BASENAME=b.e21.$COMPSET'.'$GRID'.pacemaker_pacific_xtnd'
+SCRATCHBASE="/glade/derecho/scratch/nanr/" # !!!change this
 
-cd ~nanr/CESM-WF/
-  ./create_cylc_cesm2-pacemaker-ensemble-SSP370 --case $CASEROOTBASE$BASENAME --compset $COMPSET --res $GRID --mach cheyenne --run-unsupported
 
 for imem in `seq $STARTMEMBER $ENDMEMBER` ; do
   memstr=`printf %03d $imem`
@@ -29,50 +27,36 @@ for imem in `seq $STARTMEMBER $ENDMEMBER` ; do
 
   memstr=`printf %03d $imem`
   caseroot=$CASEROOTBASE$BASENAME'.'$memstr
-  REFROOT=$INITDIR$INITNAME'.'$memstr'-b/rest/2015-01-01-00000/'
+  REFROOT=$INITDIR$INITNAME'.'$memstr'/rest/2020-01-01-00000/'
+  cd $CESMROOT/cime/scripts
+  ./create_newcase --case $caseroot --compset $COMPSET --res $GRID  --run-unsupported
   
 echo "======="
   echo $caseroot
   echo $INITDIR
   echo $INITDIR$INITNAME'.'$memstr
+  echo $REFROOT
 echo "======="
  
   cd $caseroot
   ./xmlchange RUN_TYPE=hybrid
-  ./xmlchange RUN_STARTDATE="2015-01-01"
+  ./xmlchange RUN_STARTDATE="2020-01-01"
   ./xmlchange STOP_OPTION="nmonths"
-  ./xmlchange STOP_N=30
+  ./xmlchange STOP_N=24
   ./xmlchange RESUBMIT=1
-  ./xmlchange RUN_REFDIR=$INITDIR$INITNAME'.'$memstr'-b'
+  ./xmlchange RUN_REFDIR=$INITDIR$INITNAME'.'$memstr
   ./xmlchange RUN_REFCASE=$initcase
-  ./xmlchange RUN_REFDATE="2015-01-01"
-  ./xmlchange JOB_QUEUE=regular
-
-  ./xmlchange NTASKS_ICE=36
-  ./xmlchange NTASKS_LND=504
-  ./xmlchange ROOTPE_ICE=504
-
-# ./xmlchange NTASKS_CPL=1152
-# ./xmlchange NTASKS_ATM=1152
-# ./xmlchange NTASKS_LND=864
-# ./xmlchange NTASKS_ICE=288
-# ./xmlchange NTASKS_OCN=256
-# ./xmlchange NTASKS_ROF=864
-# ./xmlchange NTASKS_GLC=1152
-# ./xmlchange NTASKS_WAV=32
-# ./xmlchange NTHRDS_CPL=3
-# ./xmlchange NTHRDS_ATM=3
-# ./xmlchange NTHRDS_LND=3
-# ./xmlchange NTHRDS_ICE=3
-# ./xmlchange NTHRDS_OCN=3
-# ./xmlchange NTHRDS_ROF=3
-# ./xmlchange NTHRDS_GLC=3
-# ./xmlchange NTHRDS_WAV=3
-# ./xmlchange ROOTPE_ICE=864
-# ./xmlchange ROOTPE_OCN=1152
-# ./xmlchange ROOTPE_WAV=1408
+  ./xmlchange RUN_REFDATE="2020-01-01"
+  ./xmlchange GET_REFCASE=FALSE
 
   ./case.setup
+
+  if ($memstr == '001') then
+      qcmd -- ./case.build
+  else
+      ./xmlchange EXEROOT="/glade/derecho/scratch/nanr/b.e21.BSSP370cmip6.f09_g17.pacemaker_pacific_xtnd.001/bld/"
+      ./xmlchange BUILD_COMPLETE=TRUE
+  fi
 
 ##copy over namelist_definitions 
 cp $SOURCEMODSDIR/user_nl_files/* $caseroot/
